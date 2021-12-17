92d2339c4f2b87181fb71178c91953e70720a125
Adaeze Dawari <78680916+Ebulor@users.noreply.github.com>
2021-12-18 02:08:48 +0400

    first

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+fetch("data.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    createComments(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 
+function createComments(data) {
+  const container = document.querySelector(".container");
+  const currentUser = document.createElement("div");
+
+  //append
+  container.appendChild(currentUser);
+
+  //set
+  currentUser.innerHTML = data[0].username;
+}
